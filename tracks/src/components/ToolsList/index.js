bd42e3a57a1afdb5706a98ee5e03208ca248aeb8
mohamadsoufi <mohamad.soufi@outlook.com>
2020-11-20 22:53:08 +0100

    added ToolsList component

---
+++ tracks/src/components/ToolsList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FilterBy from "../FilterBy";
+
+export default function ToolsList({ info, filterByCity }) {
+    return (
+        <div>
+            <p>hsdfkhsdfjh</p>
+
+            <FilterBy info={info} filterByCity={filterByCity} />
+        </div>
+    );
+}
