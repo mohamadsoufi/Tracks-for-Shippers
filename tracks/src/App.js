c0de5453b128fb5c6d3c4ea09a7cb45a03352c61
mohamadsoufi <mohamad.soufi@outlook.com>
2020-11-21 00:16:11 +0100

    edited App component

--- tracks/src/App.js
+++ tracks/src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import DisplayData from "./components/DispalayData";
+import ToolsList from "./components/ToolsList";
+import Box from "@material-ui/core/Box";
 
 function App() {
     let [info, setInfo] = useState([]);
@@ -12,13 +15,32 @@ function App() {
             });
     }, []);
 
-    if (info) {
-        info.forEach((i) => console.log("i :", i.id));
-    }
+    const filterByCity = (startCity, endCity) => {
+        fetch("/api")
+            .then((data) => data.json())
+            .then((data) => {
+                let res = data.filter((details) => {
+                    return (
+                        details.start_city === startCity &&
+                        details.end_city === endCity
+                    );
+                });
+
+                setInfo(res);
+            });
+    };
 
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <header className="App-header">header</header>
+            <main style={{ width: "100%" }}>
+                <Box display="flex" p={1}>
+                    <DisplayData info={info} />
+                    <Box p={2}>
+                        <ToolsList info={info} filterByCity={filterByCity} />
+                    </Box>
+                </Box>
+            </main>
         </div>
     );
 }
